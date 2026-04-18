a61f6a9277b1cba45a207f8dd9d1bc024fe6e92e
Nomi <xnomi555@gmail.com>
2026-04-18 15:19:40 +0500

    chore: remove test_pdf.js

--- test_pdf.js
+++
@@ -1,7 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const pdfParse = require('pdf-parse');
-console.log(typeof pdfParse);
-if (typeof pdfParse === 'object') {
-  console.log(Object.keys(pdfParse));
-}
